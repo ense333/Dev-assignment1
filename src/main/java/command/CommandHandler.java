653c79128c0ca85ed279c7ae5281b78665bfc45b
나상연 <reddynor@naver.com>
2025-03-15 23:35:36 +0900

    feat: Implement 1-1, 1-2 step

---
+++ src/main/java/command/CommandHandler.java
@@ -0,0 +1,19 @@
+package command;
+
+import java.util.Scanner;
+
+public class CommandHandler {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.print("명령어 > ");
+            String command = sc.nextLine();
+            if(command.equals("종료")){
+                System.out.println("프로그램이 종료됩니다.");
+                break;
+            }
+            System.out.println("존재하지 않는 명령어 입니다.");
+        }
+    }
+}
