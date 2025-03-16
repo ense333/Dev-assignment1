3c1284828f0e3202c969be1e83ea7c5b1a23559f
나상연 <reddynor@naver.com>
2025-03-16 16:51:43 +0900

    feat: Implement 1-6, 1-7 refactor: extract checking post method to avoid duplication

--- src/main/java/command/CommandHandler.java
+++ src/main/java/command/CommandHandler.java
@@ -27,29 +27,47 @@ public class CommandHandler {
                 System.out.print("어떤 게시물을 조회할까요? ");
                 long idx = sc.nextLong();
                 sc.nextLine();
+                if (checkAndHandle(postController, idx)) continue;
                 Post post = postController.readPost(idx);
-                if(post != null){
-                    System.out.println("번호: " + post.getPostId());
-                    System.out.println("제목: " + post.getTitle());
-                    System.out.println("내용: " + post.getContent());
-                }
+                System.out.println("번호: " + post.getPostId());
+                System.out.println("제목: " + post.getTitle());
+                System.out.println("내용: " + post.getContent());
+
             }else if(command.equals("삭제")){
                 System.out.print("어떤 게시물을 삭제할까요? ");
                 long idx = sc.nextLong();
                 sc.nextLine();
+                if (checkAndHandle(postController, idx)) continue;
                 postController.deletePost(idx);
             }else if(command.equals("수정")){
                 System.out.print("어떤 게시물을 수정할까요? ");
                 long idx = sc.nextLong();
                 sc.nextLine();
+                if (checkAndHandle(postController, idx)) continue;
                 System.out.print("수정할 제목: ");
                 String title = sc.nextLine();
                 System.out.print("수정할 내용: ");
                 String content = sc.nextLine();
-                postController.updatePost(idx, title, content);
+                postController.updatePost(idx,title,content);
+            }else if(command.equals("목록")){
+                try {
+                    postController.printAll();
+                } catch (IllegalArgumentException e){
+                    System.out.println(e.getMessage());
+                }
             }else{
                 System.out.println("존재하지 않는 명령어 입니다.");
             }
         }
+    }
+
+    private static boolean checkAndHandle(PostController postController, long idx) {
+        try {
+            postController.checkExist(idx);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/controller/PostController.java
+++ src/main/java/controller/PostController.java
@@ -16,30 +16,37 @@ public class PostController {
     }
 
     public Post readPost(long postId){
-        if(!posts.containsKey(postId)){
-            System.out.println("게시글이 비어있습니다.");
-            return null;
-        }
         return posts.get(postId);
     }
 
     public Post deletePost(long postId){
-        if(!posts.containsKey(postId)){
-            System.out.println("게시글이 비어있습니다.");
-            return null;
-        }
         System.out.println(postId + "번 게시글이 성공적으로 삭제 되었습니다.");
         return posts.remove(postId);
     }
 
     public void updatePost(long postId, String title, String content){
-        if(!posts.containsKey(postId)){
-            System.out.println("게시글이 비어있습니다.");
-            return;
-        }
         Post peek = posts.get(postId);
         peek.updatePost(title, content);
         System.out.println(postId + "번 게시글이 성공적으로 수정되었습니다.");
+    }
+
+    public void checkExist(long postId){
+        if(!posts.containsKey(postId)){
+            throw new IllegalArgumentException(postId + "번 게시글은 존재하지 않습니다.");
+        }
+    }
+
+    public void printAll(){
+        if(posts.isEmpty()){
+            throw new IllegalArgumentException("작성되어 있는 게시글이 없습니다.");
+        }
+        System.out.println("총 게시글은 " + posts.size() + "개 작성되어있습니다.");
+        for (Long idx : posts.keySet()) {
+            System.out.println(idx + "번 게시글");
+            Post post = posts.get(idx);
+            System.out.println("제목 : " + post.getTitle());
+            System.out.println("내용 : " + post.getContent());
+        }
     }
 
 
